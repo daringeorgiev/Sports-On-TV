7a4b1384007d7742ce1898a3d4605df5e37913da
darinnn <georgiev.darin@gmail.com>
2015-12-23 03:07:45 +0200

    Add startTime in createSport function.

--- webSportsOnTV/app/controllers/SportsController.js
+++ webSportsOnTV/app/controllers/SportsController.js
@@ -3,8 +3,10 @@
     var Sport = require('../models/sport');
 
     module.exports = {
-        getTodaySports : function(req, res) {
-            Sport.find({}, function(err, sports) {
+        getTodaySports: function(req, res) {
+            Sport.find({}).sort({
+                startTime: 'asc'
+            }).exec(function(err, sports) {
                 if (err) {
                     throw err;
                 }
@@ -12,16 +14,14 @@
             });
         },
 
-        createSport : function (req, res) {
+        createSport: function(req, res) {
             Sport.create({
                 sportName: req.body.sportName,
-                // ToDo
-                // sportDate: Date,
-                // startHour: new Date(req.body.startTime),
+                startTime: new Date(req.body.startTime),
                 tv: req.body.tv,
                 sportType: req.body.sportType,
                 descr: req.body.descr
-            }, function (err, sport){
+            }, function(err, sport) {
                 if (err) {
                     res.send(err);
                     throw err;
