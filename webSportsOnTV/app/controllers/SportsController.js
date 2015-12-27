baa0e5ce1b040e6b221804e757800f8a9c865ec0
darinnn <georgiev.darin@gmail.com>
2015-12-27 03:17:42 +0200

    Add getSportsByDaysDiff

--- webSportsOnTV/app/controllers/SportsController.js
+++ webSportsOnTV/app/controllers/SportsController.js
@@ -1,109 +1,135 @@
-(function() {
-    'use strict';
-    var Sport = require('../models/sport');
+var Sport = require('../models/sport');
 
-    module.exports = {
-        getTodaySports: function(req, res) {
-            var today = new Date();
-            var tomorrow = new Date();
-            tomorrow = tomorrow.setDate(tomorrow.getDate() + 1);
+module.exports = {
+    getTodaySports: function(req, res) {
+        var today = new Date();
+        var tomorrow = new Date();
+        tomorrow = tomorrow.setDate(tomorrow.getDate() + 1);
 
-            Sport.find({
-                startTime: {
-                    "$gte": today,
-                    "$lt": tomorrow
-                }
-            }).sort({
-                startTime: 'asc'
-            }).exec(function(err, sports) {
-                if (err) {
-                    throw err;
-                }
-                res.json(sports);
+        Sport.find({
+            startTime: {
+                "$gte": today,
+                "$lt": tomorrow
+            }
+        }).sort({
+            startTime: 'asc'
+        }).exec(function(err, sports) {
+            if (err) {
+                throw err;
+            }
+            res.json(sports);
+        });
+    },
+
+    getSportsByDaysDiff: function(req, res) {
+        var days = parseInt(req.query.days);
+        if (isNaN(days) || days < 0 || days > 2) {
+            res.status(501).send({
+                message: 'Invalide day diff'
             });
-        },
+            return;
+        };
 
-        getAllSports: function(req, res) {
-            Sport.find({}).sort({
-                startTime: 'asc'
-            }).exec(function(err, sports) {
-                if (err) {
-                    throw err;
-                }
-                res.json(sports);
-            });
-        },
+        var day = new Date(),
+            secondDay = new Date();
+        day = day.setDate(day.getDate() + days);
+        secondDay = secondDay.setDate(secondDay.getDate() + days + 1);
 
-        getSportById: function(req, res) {
-            Sport.find({
-                _id: req.query.id
-            }, function(err, sport) {
+        Sport.find({
+            startTime: {
+                "$gte": day,
+                "$lt": secondDay
+            }
+        }).sort({
+            startTime: 'asc'
+        }).exec(function(err, sports) {
+            if (err) {
+                throw err;
+            }
+            res.json(sports);
+        });
+    },
+
+    getAllSports: function(req, res) {
+        Sport.find({}).sort({
+            startTime: 'asc'
+        }).exec(function(err, sports) {
+            if (err) {
+                throw err;
+            }
+            res.json(sports);
+        });
+    },
+
+    getSportById: function(req, res) {
+        Sport.find({
+            _id: req.query.id
+        }, function(err, sport) {
+            if (err) {
+                res.send(err);
+                throw err;
+            }
+            res.send(sport);
+        });
+    },
+
+    createSport: function(req, res) {
+        Sport.create({
+            sportName: req.body.sportName,
+            startTime: new Date(req.body.startTime),
+            tv: req.body.tv,
+            sportType: req.body.sportType,
+            descr: req.body.descr
+        }, function(err, sport) {
+            if (err) {
+                res.send(err);
+                throw err;
+            }
+            res.send(sport);
+        });
+    },
+
+    updateSport: function(req, res) {
+        Sport.findOne({
+            _id: req.body.id
+        }, function(err, sport) {
+            if (err) {
+                res.send(err);
+                throw err;
+            }
+
+            if (!sport) {
+                return res.status(404)
+                    .send('Sport not found');
+            }
+
+            sport.sportName = req.body.sportName;
+            sport.startTime = new Date(req.body.startTime);
+            sport.tv = req.body.tv;
+            sport.sportType = req.body.sportType;
+            sport.descr = req.body.descr;
+
+            sport.save(function(err, sport) {
                 if (err) {
                     res.send(err);
                     throw err;
                 }
                 res.send(sport);
             });
-        },
+        });
+    },
 
-        createSport: function(req, res) {
-            Sport.create({
-                sportName: req.body.sportName,
-                startTime: new Date(req.body.startTime),
-                tv: req.body.tv,
-                sportType: req.body.sportType,
-                descr: req.body.descr
-            }, function(err, sport) {
-                if (err) {
-                    res.send(err);
-                    throw err;
-                }
-                res.send(sport);
+    deleteSport: function(req, res) {
+        Sport.remove({
+            _id: req.body.id
+        }, function(err) {
+            if (err) {
+                res.send(err);
+                throw err;
+            }
+            res.json({
+                message: 'Sport deleted successfully'
             });
-        },
-
-        updateSport: function(req, res) {
-            Sport.findOne({
-                _id: req.body.id
-            }, function(err, sport) {
-                if (err) {
-                    res.send(err);
-                    throw err;
-                }
-
-                if (!sport) {
-                    return res.status(404)
-                        .send('Sport not found');
-                }
-
-                sport.sportName = req.body.sportName;
-                sport.startTime = new Date(req.body.startTime);
-                sport.tv = req.body.tv;
-                sport.sportType = req.body.sportType;
-                sport.descr = req.body.descr;
-
-                sport.save(function(err, sport) {
-                    if (err) {
-                        res.send(err);
-                        throw err;
-                    }
-                    res.send(sport);
-                });
-            });
-        },
-
-        deleteSport: function(req, res) {
-            Sport.remove({
-                _id: req.body.id
-            }, function(err) {
-                if (err) {
-                    res.send(err);
-                    throw err;
-                }
-                res.json({
-                    message: 'Sport deleted successfully'
-                });
-            });
-        }
-    };
-}());
+        });
+    }
+};
--- webSportsOnTV/app/routes.js
+++ webSportsOnTV/app/routes.js
@@ -1,27 +1,27 @@
-(function() {
-    'use strict';
-    var sportsCtrl = require('./controllers/SportsController'),
-        usersCtrl = require('./controllers/UsersController'),
-        auth = require('./auth');
+var sportsCtrl = require('./controllers/SportsController'),
+    usersCtrl = require('./controllers/UsersController'),
+    auth = require('./auth');
 
-    module.exports = function(app) {
-        // Sports ===============================================
-        app.get('/api/sports/today', sportsCtrl.getTodaySports);
-        app.get('/api/sports/getById', sportsCtrl.getSportById);
-        app.get('/api/sports/getAll', sportsCtrl.getAllSports);
-        app.post('/api/sports/create', sportsCtrl.createSport);
-        app.put('/api/sports/update', sportsCtrl.updateSport);
-        app.post('/api/sports/delete', sportsCtrl.deleteSport);
+module.exports = function(app) {
+    // Sports ===============================================
+    app.get('/api/sports/today', sportsCtrl.getTodaySports);
+    app.get('/api/sports/getByDaysDiff', sportsCtrl.getSportsByDaysDiff);
 
-        // Users =================================================
-        app.post('/api/users/login', usersCtrl.userLogin);
-        app.post('/api/users/register', usersCtrl.userRegister);
-        app.get('/api/users/getAll', usersCtrl.getAllUsers);
 
-        // Index =================================================
-        app.get('/', function(req, res) {
-            // res.sendFile(__dirname + '/public/index.html');
-            res.send('index file');
-        });
-    };
-}());
+    app.get('/api/sports/getById', sportsCtrl.getSportById);
+    app.get('/api/sports/getAll', sportsCtrl.getAllSports);
+    app.post('/api/sports/create', sportsCtrl.createSport);
+    app.put('/api/sports/update', sportsCtrl.updateSport);
+    app.post('/api/sports/delete', sportsCtrl.deleteSport);
+
+    // Users =================================================
+    app.post('/api/users/login', usersCtrl.userLogin);
+    app.post('/api/users/register', usersCtrl.userRegister);
+    app.get('/api/users/getAll', usersCtrl.getAllUsers);
+
+    // Index =================================================
+    app.get('/', function(req, res) {
+        // res.sendFile(__dirname + '/public/index.html');
+        res.send('index file');
+    });
+};
