a0897e6060c5586db5902f97844dae6d5bcda771
darinnn <georgiev.darin@gmail.com>
2016-01-08 02:47:53 +0200

    Add getTodaySports in controller.

--- mobileSportsOnTV/www/js/controllers/sportsController.js
+++ mobileSportsOnTV/www/js/controllers/sportsController.js
@@ -1,6 +1,20 @@
 (function(){
     'use strict';
-    app.controller('SportsCOntroller', ['sportsService', function(sportsService){
+    app.controller('SportsController', ['sportsService', function(sportsService){
         var self = this;
+
+        self.getTodaySports = function(){
+            sportsService.getTodaySports()
+                .then(function successCallback(response) {
+                    self.todaySports = response;
+                    console.log(self.todaySports);
+                // this callback will be called asynchronously
+                // when the response is available
+                }, function errorCallback(response) {
+                    console.log(response);
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                });
+        };
     }])
 }())
--- mobileSportsOnTV/www/js/services/sportsService.js
+++ mobileSportsOnTV/www/js/services/sportsService.js
@@ -1,12 +1,16 @@
 (function(){
     'use strict';
     app.factory('sportsService', ['$http', function($http){
+        var self = this,
+            baseUrl = 'http://localhost:8080'
+
         function getTodaySports() {
-            return $http.get('/api/sports/getByDaysDiff' + '?days=' + 0)
+            return $http.get(baseUrl + '/api/sports/getAll');
+            // return $http.get(baseUrl + '/api/sports/getByDaysDiff' + '?days=' + 0);
         };
 
         function getTomorrowSports() {
-            return $http.get('/api/sports/getByDaysDiff' + '?days=' + 1)
+            return $http.get(baseUrl + '/api/sports/getByDaysDiff' + '?days=' + 1)
         };
 
         return {
